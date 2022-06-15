1f6f6dd8979a8bca45c770f2481966c5bca05f50
jA71 <isjavier77@gmail.com>
2022-06-15 18:00:38 -0500

    Se agrega menu de opciones

--- app/src/main/java/com/example/findatruck/interface_user/ListProduct.kt
+++ app/src/main/java/com/example/findatruck/interface_user/ListProduct.kt
@@ -1,10 +1,15 @@
 package com.example.findatruck.interface_user
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.findatruck.Adaptador
+import com.example.findatruck.R
 import com.example.findatruck.databinding.ActivityListProductBinding
 import com.example.findatruck.dto.Product
 import com.example.findatruck.dto.WsClient
@@ -23,17 +28,47 @@ class ListProduct : AppCompatActivity() {
         addListProducts()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.share_list -> {
+                Toast.makeText(this, "Quiero Compartir", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.settings_list -> {
+                val intent = Intent(this, Preferences::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.example_list -> {
+                Toast.makeText(this,"Hola Ejemplo", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                true
+        }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
+
     private fun initialConfiguration() {
         views.listItems.layoutManager = LinearLayoutManager(this)
     }
 
+
     private fun addListProducts() {
         WsClient.apiLista()?.buscarListaProductos()?.enqueue(object : Callback<List<Product>> {
             override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
-                if (response.isSuccessful){
+                if (response.isSuccessful) {
                     val list = response.body()!!
                     views.listItems.adapter = Adaptador(list)
-                }else{
+                } else {
                     Toast.makeText(
                         this@ListProduct,
                         android.R.string.httpErrorBadUrl,
---
+++ app/src/main/java/com/example/findatruck/interface_user/Preferences.kt
@@ -0,0 +1,12 @@
+package com.example.findatruck.interface_user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.findatruck.R
+
+class Preferences : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+}
